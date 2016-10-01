d82ec568c2923b8dc9d5e09d55f9f17e5846fb41
Titan <howay.tan@gmail.com>
2016-10-01 16:43:25 +0800

    Check parameters

--- src/processor.ts
+++ src/processor.ts
@@ -1,58 +1,58 @@
-import { Processor, Config, ModuleFunction, DoneFunction, async_serial_ignore } from 'hive-processor';
-import { Client as PGClient, ResultSet } from 'pg';
-import { RedisClient} from 'redis';
-import * as bunyan from 'bunyan';
+import { Processor, Config, ModuleFunction, DoneFunction, async_serial_ignore } from "hive-processor";
+import { Client as PGClient, ResultSet } from "pg";
+import { RedisClient} from "redis";
+import * as bunyan from "bunyan";
 
 let log = bunyan.createLogger({
-  name: 'plan-processor',
+  name: "plan-processor",
   streams: [
     {
-      level: 'info',
-      path: '/var/log/plan-processor-info.log',  // log ERROR and above to a file
-      type: 'rotating-file',
-      period: '1d',   // daily rotation
+      level: "info",
+      path: "/var/log/plan-processor-info.log",  // log ERROR and above to a file
+      type: "rotating-file",
+      period: "1d",   // daily rotation
       count: 7        // keep 7 back copies
     },
     {
-      level: 'error',
-      path: '/var/log/plan-processor-error.log',  // log ERROR and above to a file
-      type: 'rotating-file',
-      period: '1w',   // daily rotation
+      level: "error",
+      path: "/var/log/plan-processor-error.log",  // log ERROR and above to a file
+      type: "rotating-file",
+      period: "1w",   // daily rotation
       count: 3        // keep 7 back copies
     }
   ]
 });
 
 let config: Config = {
-  dbhost: process.env['DB_HOST'],
-  dbuser: process.env['DB_USER'],
-  dbport: process.env['DB_PORT'],
-  database: process.env['DB_NAME'],
-  dbpasswd: process.env['DB_PASSWORD'],
-  cachehost: process.env['CACHE_HOST'],
+  dbhost: process.env["DB_HOST"],
+  dbuser: process.env["DB_USER"],
+  dbport: process.env["DB_PORT"],
+  database: process.env["DB_NAME"],
+  dbpasswd: process.env["DB_PASSWORD"],
+  cachehost: process.env["CACHE_HOST"],
   addr: "ipc:///tmp/plan.ipc"
 };
 
 let processor = new Processor(config);
 
-processor.call('refresh', (db: PGClient, cache: RedisClient, done: DoneFunction) => {
-  log.info('refresh');
-  db.query('SELECT p.id AS p_id, p.title AS p_title, p.description AS p_description, p.image AS p_image, p.thumbnail AS p_thumbnail, p.period AS p_period, p.show_in_index AS p_show_in_index, pr.id AS pr_id, pr.name AS pr_name, pr.title AS pr_title, pr.description AS pr_description FROM plans AS p LEFT JOIN plan_rules AS pr ON p.id = pr.pid', [], (err: Error, result: ResultSet) => {
+processor.call("refresh", (db: PGClient, cache: RedisClient, done: DoneFunction) => {
+  log.info("refresh");
+  db.query("SELECT p.id AS p_id, p.title AS p_title, p.description AS p_description, p.image AS p_image, p.thumbnail AS p_thumbnail, p.period AS p_period, p.show_in_index AS p_show_in_index, pr.id AS pr_id, pr.name AS pr_name, pr.title AS pr_title, pr.description AS pr_description FROM plans AS p LEFT JOIN plan_rules AS pr ON p.id = pr.pid", [], (err: Error, result: ResultSet) => {
     if (err) {
-      log.error(err, 'query error');
+      log.error(err, "query error");
       return;
     }
     let plans = [];
     let last_pid = null;
     let plan = null;
     for (let row of result.rows) {
-      if (row.p_id != last_pid) {
+      if (row.p_id !== last_pid) {
         plan = {
           id: row.p_id,
-          title: row.p_title? row.p_title.trim(): '',
+          title: row.p_title ? row.p_title.trim() : "",
           description: row.p_description,
-          image: row.p_image? row.p_image.trim(): '',
-          thumbnail: row.p_thumbnail? row.p_thumbnail.trim(): '',
+          image: row.p_image ? row.p_image.trim() : "",
+          thumbnail: row.p_thumbnail ? row.p_thumbnail.trim() : "",
           period: row.p_period,
           show_in_index: row.p_show_in_index,
           rules: [],
@@ -65,8 +65,8 @@ processor.call('refresh', (db: PGClient, cache: RedisClient, done: DoneFunction)
         if (row.pr_id != null) {
           let rule = {
             id: row.pr_id,
-            name: row.pr_name? row.pr_name.trim(): '',
-            title: row.pr_title? row.pr_title.trim(): '',
+            name: row.pr_name ? row.pr_name.trim() : "",
+            title: row.pr_title ? row.pr_title.trim() : "",
             description: row.pr_description
           };
           plan.rules.push(rule);
@@ -75,7 +75,7 @@ processor.call('refresh', (db: PGClient, cache: RedisClient, done: DoneFunction)
     }
     let ps = plans.map(plan => {
       return new Promise<Object>((resolve, reject) => {
-        db.query('SELECT id, title, description FROM plan_items WHERE pid = $1', [ plan.id ], (err1: Error, result1: ResultSet) => {
+        db.query("SELECT id, title, description FROM plan_items WHERE pid = $1", [ plan.id ], (err1: Error, result1: ResultSet) => {
           if (err1) {
             reject(err1);
           } else {
@@ -108,11 +108,11 @@ processor.call('refresh', (db: PGClient, cache: RedisClient, done: DoneFunction)
 function row2item(row) {
   return {
     id: row.id,
-    title: row.title? row.title.trim(): '',
+    title: row.title ? row.title.trim() : "",
     description: row.description
   };
 }
 
-log.info('Start processor at %s', config.addr);
+log.info("Start processor at %s", config.addr);
 
 processor.run();
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,26 @@
-import { Server, Config, Context, ResponseFunction, Permission } from 'hive-server';
+import { Server, Config, Context, ResponseFunction, Permission } from "hive-server";
 import * as Redis from "redis";
-import * as nanomsg from 'nanomsg';
-import * as msgpack from 'msgpack-lite';
-import * as bunyan from 'bunyan';
+import * as nanomsg from "nanomsg";
+import * as msgpack from "msgpack-lite";
+import * as bunyan from "bunyan";
 import { servermap } from "hive-hostmap";
+import { verify, uuidVerifier, arrayVerifier } from "hive-verify";
 
 let log = bunyan.createLogger({
-  name: 'plan-server',
+  name: "plan-server",
   streams: [
     {
-      level: 'info',
-      path: '/var/log/plan-server-info.log',  // log ERROR and above to a file
-      type: 'rotating-file',
-      period: '1d',   // daily rotation
+      level: "info",
+      path: "/var/log/plan-server-info.log",  // log ERROR and above to a file
+      type: "rotating-file",
+      period: "1d",   // daily rotation
       count: 7        // keep 7 back copies
     },
     {
-      level: 'error',
-      path: '/var/log/plan-server-error.log',  // log ERROR and above to a file
-      type: 'rotating-file',
-      period: '1w',   // daily rotation
+      level: "error",
+      path: "/var/log/plan-server-error.log",  // log ERROR and above to a file
+      type: "rotating-file",
+      period: "1w",   // daily rotation
       count: 3        // keep 7 back copies
     }
   ]
@@ -31,16 +32,16 @@ let entity_key = "plan-entities";
 
 let config: Config = {
   svraddr: servermap["plan"],
-  msgaddr: 'ipc:///tmp/plan.ipc',
+  msgaddr: "ipc:///tmp/plan.ipc",
   cacheaddr: process.env["CACHE_HOST"]
 };
 
 let svr = new Server(config);
 
-let allowall: Permission[] = [['mobile', true], ['admin', true]];
+let allowall: Permission[] = [["mobile", true], ["admin", true]];
 
-svr.call('getAvailablePlans', allowall, (ctx: Context, rep: ResponseFunction) => {
-  log.info('getAvailablePlans uid: %s', ctx.uid);
+svr.call("getAvailablePlans", allowall, (ctx: Context, rep: ResponseFunction) => {
+  log.info("getAvailablePlans uid: %s", ctx.uid);
   // http://redis.io/commands/sdiff
   ctx.cache.sdiff(list_key, entities_prefix + ctx.uid, function (err, result) {
     if (err) {
@@ -51,8 +52,8 @@ svr.call('getAvailablePlans', allowall, (ctx: Context, rep: ResponseFunction) =>
   });
 });
 
-svr.call('getJoinedPlans', allowall, (ctx: Context, rep: ResponseFunction) => {
-  log.info('getJoinedPlans uid: %s', ctx.uid);
+svr.call("getJoinedPlans", allowall, (ctx: Context, rep: ResponseFunction) => {
+  log.info("getJoinedPlans uid: %s", ctx.uid);
   // http://redis.io/commands/smembers
   ctx.cache.smembers(entities_prefix + ctx.uid, function (err, result) {
     if (err) {
@@ -63,26 +64,48 @@ svr.call('getJoinedPlans', allowall, (ctx: Context, rep: ResponseFunction) => {
   });
 });
 
-svr.call('getPlan', allowall, (ctx: Context, rep: ResponseFunction, pid: string) => {
-  log.info('getPlan uid: %s, pid: %s', ctx.uid, pid);
+svr.call("getPlan", allowall, (ctx: Context, rep: ResponseFunction, pid: string) => {
+  log.info("getPlan uid: %s, pid: %s", ctx.uid, pid);
+  if (!verify([uuidVerifier("pid", pid)], (errors: string[]) => {
+    rep({
+      code: 400,
+      msg: errors.join("\n")
+    });
+  })) {
+    return;
+  }
   // http://redis.io/commands/hget
   ctx.cache.hget(entity_key, pid, (err, planstr) => {
     if (err) {
-      rep(null);
+      rep({
+        code: 500,
+        msg: err.message
+      });
     } else if (planstr) {
       let plan = JSON.parse(planstr);
-      ctx.cache.hget('plan-joined-count', plan.id, (err, count) => {
-        plan.joinedCount = count? count: 0;
+      ctx.cache.hget("plan-joined-count", plan.id, (err, count) => {
+        plan.joinedCount = count ? count : 0;
         rep(plan);
       });
     } else {
-      rep(null);
+      rep({
+        code: 404,
+        msg: "Plan not found"
+      });
     }
   });
 });
 
-svr.call('increaseJoinedCount', allowall, (ctx: Context, rep: ResponseFunction, pid: string) => {
-  log.info('increaseJoinedCount uid: %s, pid: %s', ctx.uid, pid);
+svr.call("increaseJoinedCount", allowall, (ctx: Context, rep: ResponseFunction, pid: string) => {
+  log.info("increaseJoinedCount uid: %s, pid: %s", ctx.uid, pid);
+  if (!verify([uuidVerifier("pid", pid)], (errors: string[]) => {
+    rep({
+      code: 400,
+      msg: errors.join("\n")
+    });
+  })) {
+    return;
+  }
   ctx.cache.hincrby("plan-joined-count", pid, 1, (err, count) => {
     if (err) {
       rep({
@@ -95,8 +118,16 @@ svr.call('increaseJoinedCount', allowall, (ctx: Context, rep: ResponseFunction, 
   });
 });
 
-svr.call('decreaseJoinedCount', allowall, (ctx: Context, rep: ResponseFunction, pid: string) => {
-  log.info('decreaseJoinedCount uid: %s, pid: %s', ctx.uid, pid);
+svr.call("decreaseJoinedCount", allowall, (ctx: Context, rep: ResponseFunction, pid: string) => {
+  log.info("decreaseJoinedCount uid: %s, pid: %s", ctx.uid, pid);
+  if (!verify([uuidVerifier("pid", pid)], (errors: string[]) => {
+    rep({
+      code: 400,
+      msg: errors.join("\n")
+    });
+  })) {
+    return;
+  }
   ctx.cache.hincrby("plan-joined-count", pid, -1, (err, count) => {
     if (err) {
       rep({
@@ -109,8 +140,16 @@ svr.call('decreaseJoinedCount', allowall, (ctx: Context, rep: ResponseFunction, 
   });
 });
 
-svr.call('setJoinedCounts', allowall, (ctx: Context, rep: ResponseFunction, params: [string, number][]) => {
-  log.info('setJoinedCounts uid: %s, params: %j', ctx.uid, params);
+svr.call("setJoinedCounts", allowall, (ctx: Context, rep: ResponseFunction, params: [string, number][]) => {
+  log.info("setJoinedCounts uid: %s, params: %j", ctx.uid, params);
+  if (!verify([arrayVerifier("params", params)], (errors: string[]) => {
+    rep({
+      code: 400,
+      msg: errors.join("\n")
+    });
+  })) {
+    return;
+  }
   let multi = ctx.cache.multi();
   for (let [pid, count] of params) {
     multi.hset("plan-joined-count", pid, count);
@@ -130,10 +169,10 @@ svr.call('setJoinedCounts', allowall, (ctx: Context, rep: ResponseFunction, para
   });
 });
 
-svr.call('refresh', allowall, (ctx: Context, rep: ResponseFunction) => {
-  log.info('refresh uid: %s', ctx.uid);
+svr.call("refresh", allowall, (ctx: Context, rep: ResponseFunction) => {
+  log.info("refresh uid: %s", ctx.uid);
   ctx.msgqueue.send(msgpack.encode({cmd: "refresh", args: null}));
-  rep({status: 'okay'});
+  rep({status: "okay"});
 });
 
 function ids2plans(ctx: Context, ids: string[], rep: ResponseFunction) {
@@ -148,13 +187,13 @@ function ids2plans(ctx: Context, ids: string[], rep: ResponseFunction) {
       let plans = [];
       for (let planstr of planstrs) {
         let plan = JSON.parse(planstr);
-        multi.hget('plan-joined-count', plan.id);
+        multi.hget("plan-joined-count", plan.id);
         plans.push(plan);
       }
       multi.exec((err, counts) => {
         for (let i in counts) {
           let p = plans[i];
-          p.joined_count = counts[i]? counts[i]: 0;
+          p.joined_count = counts[i] ? counts[i] : 0;
         }
         rep(plans);
       });
@@ -162,6 +201,6 @@ function ids2plans(ctx: Context, ids: string[], rep: ResponseFunction) {
   });
 }
 
-log.info('Start server at %s and connect to %s', config.svraddr, config.msgaddr);
+log.info("Start server at %s and connect to %s", config.svraddr, config.msgaddr);
 
 svr.run();
